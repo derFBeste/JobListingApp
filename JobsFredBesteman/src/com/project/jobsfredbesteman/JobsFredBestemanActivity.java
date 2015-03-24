9bc20a0688cebd0a77baf89a38769143502ad2d9
derFBeste <freddybeste@gmail.com>
2015-03-24 15:31:00 -0400

    Basic setup

--- JobsFredBesteman/src/com/project/jobsfredbesteman/JobsFredBestemanActivity.java
+++ JobsFredBesteman/src/com/project/jobsfredbesteman/JobsFredBestemanActivity.java
@@ -1,6 +1,7 @@
 package com.project.jobsfredbesteman;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,5 +12,22 @@ public class JobsFredBestemanActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_jobs_fred_besteman);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		this.getMenuInflater().inflate(R.menu.options_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch(item.getItemId()){
+		case R.id.about:
+			startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
