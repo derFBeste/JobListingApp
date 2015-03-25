26d0f5b8288cc08010770fc84ec5e9165a767834
besteman <besteman@10.0.1.9>
2015-03-24 20:47:17 -0400

    added adapter

--- JobsFredBesteman/src/com/project/jobsfredbesteman/JobsFredBestemanActivity.java
+++ JobsFredBesteman/src/com/project/jobsfredbesteman/JobsFredBestemanActivity.java
@@ -1,17 +1,51 @@
 package com.project.jobsfredbesteman;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class JobsFredBestemanActivity extends Activity {
+	
+	ListView listView;
+	
+	ArrayList<Job> jobList = new ArrayList<Job>();
+	ArrayAdapter<Job> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_jobs_fred_besteman);
+		
+		listView = (ListView) findViewById(R.id.jobList);
+		
+		Job job1 = new Job("ACME", "DAVE", "Head Honcho");
+		Job job2 = new Job("MMMM", "Jojo", "Numero Uno");
+		
+		jobList.add(job1);
+		jobList.add(job2);
+		
+		adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, jobList);	
+		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View v, int position,
+					long id) {
+				Job j = (Job) adapter.getItem(position);
+				Toast.makeText(getApplicationContext(), position + ": "+ j, Toast.LENGTH_LONG).show();			
+			}
+			
+		});
+		
 	}
 	
 	@Override
@@ -30,4 +64,6 @@ public class JobsFredBestemanActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
+	
+	
 }
